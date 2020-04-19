c834257d1b2fd143bcd8733f501eb04730640b05
gesab001 <144000giovanni@gmail.com>
2020-04-19 23:19:53 +1200

    added ability to receive post requests for different news

--- headlines2.py
+++ headlines2.py
@@ -1,44 +1,34 @@
 #!/usr/bin/python3
 from subprocess import call
 import cgitb
+import cgi
+
 import subprocess
 import urllib.request
 import requests
 #import mysql.connector as conn
 cgitb.enable()    
-
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+#
+# # Get data from fields
+# #first_name = form.getvalue('first_name')
+# #last_name  = form.getvalue('last_name')
+news = form.getvalue('news')
 
 print("Content-Type: text/html;charset=utf-8")
 print ("Content-type:text/html\r\n\r\n")
-rss = {"rss":[{"cnn": "http://rss.cnn.com/rss/edition.rss"},
-              {"nzherald": "https://rss.nzherald.co.nz/rss/xml/nzhtsrsscid_000000698.xml"}]}
-url = rss["rss"][1]["nzherald"]
-#print(url)
-html = ""
-r = requests.get(url)
-with urllib.request.urlopen(url ) as response:
-   html = response.read()
-#text = unicode(str(r.text), errors='ignore')
-#print ((r.text))
-#print(str(html))
-def beginning(string):
-    list = string.split(" ")
-    for x in list:
-       print (x)
-       print ("<br>")
-
-string = "In the beginning God created the heaven and the earth. Genesis 1:1"
-#beginning(string)
-#subprocess.call("poweroff", shell=True)
-
-#f = open("nzherald.xml", "w")
-#result = html
-#f.write(str(html))
+rss = {"cnn": "http://rss.cnn.com/rss/edition.rss",
+        "nzherald": "https://rss.nzherald.co.nz/rss/xml/nzhtsrsscid_000000698.xml",
+        "smh": "https://www.smh.com.au/rss/feed.xml"}
 
 
-URL = url
+url= rss[news]
 
-response = requests.get(URL)
-#print(response.text)
-with open('../html/headlines.onecloudapps.net/nzherald.xml', 'wb') as file:
+response = requests.get(url)
+print(response.text)
+try:
+ with open('../html/headlines.onecloudapps.net/'+news+'.xml', 'wb') as file:
     file.write(response.content)
+except Exception as ex:
+    print(ex)
