ba5c75972bf833bdc0a96da7cd246c609ecc0e99
gesab001 <144000giovanni@gmail.com>
2020-04-27 13:50:36 +1200

    edited hello

--- hello.py
+++ hello.py
@@ -10,16 +10,7 @@ cgitb.enable()
 
 print("Content-Type: text/html;charset=utf-8")
 print ("Content-type:text/html\r\n\r\n")
-rss = {"rss":[{"cnn": "http://rss.cnn.com/rss/edition.rss"},
-              {"nzherald": "https://rss.nzherald.co.nz/rss/xml/nzhtsrsscid_000000698.xml"}]}
-url = rss["rss"][1]["nzherald"]
-print(url)
-html = ""
 
-with urllib.request.urlopen(url ) as response:
-   html = response.read()
-
-print(str(html))
 def beginning(string):
     list = string.split(" ")
     for x in list:
@@ -28,4 +19,4 @@ def beginning(string):
 
 string = "In the beginning God created the heaven and the earth. Genesis 1:1"
 beginning(string)
-subprocess.call("poweroff", shell=True)
+
